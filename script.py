c23c11a188c2296e2e29d7805066db7d1f8b932f
Mariana Murillo <mariana@primarix.com>
2022-11-25 18:52:26 -0600

    SalesRecord_Items Table (NoAm) ready

--- script.py
+++ script.py
@@ -1,11 +1,8 @@
 import pandas as pd
-import csv
 
 table_name = input("Please enter the name of the table: ")
-#number_of_rows = int(input("Please enter the number of rows: "))
-#print("Generating " + str(table_name) + ".csv with " + str(number_of_rows)+" rows...")
 
-datasets = ["holidays_events.csv", "items.csv", "oil.csv","stores.csv","test.csv","train.csv","transactions.csv"]
+datasets = ["holidays_events.csv", "items.csv", "oil.csv","stores.csv","bloque1.csv","bloque2.csv","bloque3.csv","bloque4.csv","bloque5.csv","transactions.csv"]
 
 ## NoAm Model
 if table_name == "Store_Holidays_SalesRecord":
@@ -33,34 +30,64 @@ elif table_name == "Store_OilPrice":
     Store_OilPriceTable.rename(columns=names).to_csv('NoAm/'+file_name,columns=["store_nbr","oil_measure_date","oil_price", "store_transaction_count"], index=False, float_format='%.0f')
 
 elif table_name == "SalesRecord_Items":
-    file_name = table_name + ".csv"
-    print("Generating " + file_name + "...")
-    for csv2 in datasets:
-        if csv2 == "items.csv":
+    
+    for csv in datasets:
+        if csv == "items.csv":
             print("Reading items.csv...")
-            items_columns = pd.read_csv(csv2, usecols=["item_nbr","family","perishable"])
-        if csv2 == "train.csv":
-            print("Reading train.csv...")
-            
-        with open('train.csv') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
-            line_count = 0
-            for row in csv_reader:
-                if(line_count < 100000):
-                    if line_count == 0:
-                        print(f'Column names are {", ".join(row)}')
-                        line_count += 1
-                    else:
-                        print(f'\t{row[0]} , {row[1]} , {row[2]}, {row[3]}, {row[4]}, {row[5]}')
-                        line_count += 1
-                else:
-                    print('primer bloque')
-                    exit()
-            print(f'Processed {line_count} lines.')
-            
+            items_columns = pd.read_csv(csv, usecols=["item_nbr","family","perishable"])
+        if csv == "bloque1.csv":
+            print("Reading train.csv → 1st block...")
+            train_columns1 = pd.read_csv(csv, usecols=["id","date","store_nbr","item_nbr","unit_sales","onpromotion"])
+        if csv == "bloque2.csv":
+            print("Reading train.csv → 2nd block...")
+            train_columns2 = pd.read_csv(csv, usecols=["id","date","store_nbr","item_nbr","unit_sales","onpromotion"])
+        if csv == "bloque3.csv":
+            print("Reading train.csv → 2nd block...")
+            train_columns3 = pd.read_csv(csv, usecols=["id","date","store_nbr","item_nbr","unit_sales","onpromotion"])
+        if csv == "bloque4.csv":
+            print("Reading train.csv → 2nd block...")
+            train_columns4 = pd.read_csv(csv, usecols=["id","date","store_nbr","item_nbr","unit_sales","onpromotion"])    
+        if csv == "bloque5.csv":
+            print("Reading train.csv → 2nd block...")
+            train_columns5 = pd.read_csv(csv, usecols=["id","date","store_nbr","item_nbr","unit_sales","onpromotion"])  
     print("Finish reading train.csv")
-    # SalesRecord_ItemsTable = items_columns.merge(train_columns, how='right',on='item_nbr')
-    # names = {'id': 'record_id', 'date':'record_date','perishable':'item_perishable','family':'item_family','onpromotion':'record_on_promotion','unit_sales':'record_unit_sales','store_nbr':'store_number'}
-    # SalesRecord_ItemsTable.rename(columns=names).to_csv('NoAm/'+file_name,columns=["item_nbr","record_id","record_date","item_perishable","item_family","record_on_promotion","record_unit_sales","store_number"], index=False)
+    # Block 1
+    block_number = "1"
+    file_name = table_name + block_number + ".csv"
+    print("Generating " + file_name + "...")
+    SalesRecord_ItemsTable1 = items_columns.merge(train_columns1, how='right',on='item_nbr')
+    names = {'id': 'record_id', 'date':'record_date','perishable':'item_perishable','family':'item_family','onpromotion':'record_on_promotion','unit_sales':'record_unit_sales','store_nbr':'store_number'}
+    SalesRecord_ItemsTable1.rename(columns=names).to_csv('NoAm/'+file_name,columns=["item_nbr","record_id","record_date","item_perishable","item_family","record_on_promotion","record_unit_sales","store_number"], index=False)
+    # Block 2
+    block_number = "2"
+    file_name = table_name + block_number + ".csv"
+    print("Generating " + file_name + "...")
+    SalesRecord_ItemsTable2 = items_columns.merge(train_columns2, how='right',on='item_nbr')
+    names = {'id': 'record_id', 'date':'record_date','perishable':'item_perishable','family':'item_family','onpromotion':'record_on_promotion','unit_sales':'record_unit_sales','store_nbr':'store_number'}
+    SalesRecord_ItemsTable2.rename(columns=names).to_csv('NoAm/'+file_name,columns=["item_nbr","record_id","record_date","item_perishable","item_family","record_on_promotion","record_unit_sales","store_number"], index=False)
+    # Block 3
+    block_number = "3"
+    file_name = table_name + block_number + ".csv"
+    print("Generating " + file_name + "...")
+    SalesRecord_ItemsTable3 = items_columns.merge(train_columns3, how='right',on='item_nbr')
+    names = {'id': 'record_id', 'date':'record_date','perishable':'item_perishable','family':'item_family','onpromotion':'record_on_promotion','unit_sales':'record_unit_sales','store_nbr':'store_number'}
+    SalesRecord_ItemsTable3.rename(columns=names).to_csv('NoAm/'+file_name,columns=["item_nbr","record_id","record_date","item_perishable","item_family","record_on_promotion","record_unit_sales","store_number"], index=False)
+    # Block 4
+    block_number = "4"
+    file_name = table_name + block_number + ".csv"
+    print("Generating " + file_name + "...")
+    SalesRecord_ItemsTable4 = items_columns.merge(train_columns4, how='right',on='item_nbr')
+    names = {'id': 'record_id', 'date':'record_date','perishable':'item_perishable','family':'item_family','onpromotion':'record_on_promotion','unit_sales':'record_unit_sales','store_nbr':'store_number'}
+    SalesRecord_ItemsTable4.rename(columns=names).to_csv('NoAm/'+file_name,columns=["item_nbr","record_id","record_date","item_perishable","item_family","record_on_promotion","record_unit_sales","store_number"], index=False)
+    # Block 5
+    block_number = "5"
+    file_name = table_name + block_number + ".csv"
+    print("Generating " + file_name + "...")
+    SalesRecord_ItemsTable5 = items_columns.merge(train_columns5, how='right',on='item_nbr')
+    names = {'id': 'record_id', 'date':'record_date','perishable':'item_perishable','family':'item_family','onpromotion':'record_on_promotion','unit_sales':'record_unit_sales','store_nbr':'store_number'}
+    SalesRecord_ItemsTable5.rename(columns=names).to_csv('NoAm/'+file_name,columns=["item_nbr","record_id","record_date","item_perishable","item_family","record_on_promotion","record_unit_sales","store_number"], index=False)
+    
+    print("Finish reading train.csv")
+#NoSe model
 else:
     print("Invalid table name. ")
